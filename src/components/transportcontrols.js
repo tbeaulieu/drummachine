fcbd373ec384595868d062c905150dcc8287387b
Your Name <pixelpastry@gmail.com>
2018-12-08 15:57:08 -0500

    Padding change to pad, title and title label added

--- src/components/transportcontrols.js
+++ src/components/transportcontrols.js
@@ -5,10 +5,10 @@ class TransportControls extends Component{
     render(){
         return(
             <div className="transport-controls-wrapper">
-            <p>INTERACTIVE DEMO</p>
-            <div classId="title-label">Start by clicking on any of these buttons!</div>
+             <div class="title-label"><p>INTERACTIVE DEMO</p></div>
+            <div class="title">Start by clicking on any of these buttons!</div>
             <br />
-                <p>Tempo: {this.props.bpm} bpm<input type="range" defaultValue={this.props.bpm} onChange={this.props.changeTempo} min="20" max="300"/> </p>
+                <p>Tempo: {this.props.bpm} bpm <br /><input type="range" defaultValue={this.props.bpm} onChange={this.props.changeTempo} min="20" max="300"/> </p>
                 <button className="stop" onClick={this.props.stopSequencer}>stop</button> 
                 <button className="play" onClick={this.props.startSequencer} data-active={this.props.playstate===1 ? true : false}>play</button>
                 <p>{this.props.transportTime}</p>
