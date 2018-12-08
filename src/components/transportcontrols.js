33080519ad8845c32da7770f83adc83512ca5d81
Your Name <pixelpastry@gmail.com>
2018-12-08 15:43:40 -0500

    Updates to slider control

--- src/components/transportcontrols.js
+++ src/components/transportcontrols.js
@@ -8,7 +8,7 @@ class TransportControls extends Component{
             <p>INTERACTIVE DEMO</p>
             <div classId="title-label">Start by clicking on any of these buttons!</div>
             <br />
-                <p>Tempo: <input type="range" defaultValue={this.props.bpm} onChange={this.props.changeTempo} min="20" max="300"/> {this.props.bpm} bpm</p>
+                <p>Tempo: {this.props.bpm} bpm<input type="range" defaultValue={this.props.bpm} onChange={this.props.changeTempo} min="20" max="300"/> </p>
                 <button className="stop" onClick={this.props.stopSequencer}>stop</button> 
                 <button className="play" onClick={this.props.startSequencer} data-active={this.props.playstate===1 ? true : false}>play</button>
                 <p>{this.props.transportTime}</p>
